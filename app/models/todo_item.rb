231a6bacea25b36836cc52d5c7ea8691f3bf4ca1
Andreza Feu <andrezafeu@gmail.com>
2016-06-07 15:31:09 -0400

    Remove Mark Complete option from item that has already bees completed

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -3,4 +3,9 @@ class TodoItem < ActiveRecord::Base
 
   validates :content, presence: true,
   					  length: { minimum: 2 }
+
+  def completed?
+  	# when completed_at is not blank returns false
+  	!completed_at.blank?
+  end
 end
--- spec/features/todo_items/complete_spec.rb
+++ spec/features/todo_items/complete_spec.rb
@@ -13,4 +13,28 @@ describe "Completing todo items" do
 		todo_item.reload
 		expect(todo_item.completed_at).to_not be_nil
 	end
+
+	context "with completed items" do
+		let!(:completed_todo_item) { todo_list.todo_items.create(content: "Eggs",
+		 completed_at: 5.minutes.ago)}
+		
+		it "shows the completed items as complete." do
+			visit_todo_list(todo_list)
+			within dom_id_for(completed_todo_item) do
+				expect(page).to have_content(completed_todo_item.completed_at)
+			end
+		end
+
+		it "does not give the option to mark complete" do
+			visit_todo_list(todo_list)
+			within dom_id_for(completed_todo_item) do
+				expect(page).to_not have_content("Mark Complete")
+			end
+		end
+
+		it "returns true when completed_at is not empty" do
+			todo_item.completed_at = Time.now
+			expect(todo_item.completed?).to be_true
+		end
+	end
 end
