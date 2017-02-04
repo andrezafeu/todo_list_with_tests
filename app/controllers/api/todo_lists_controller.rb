3c6c6d88ebb00df89a918a6dad9c6c77cbc01427
Andreza Feu <andrezafeu@gmail.com>
2017-02-04 16:34:46 -0500

    Add action show for api

--- app/controllers/api/todo_lists_controller.rb
+++ app/controllers/api/todo_lists_controller.rb
@@ -1,6 +1,13 @@
 class Api::TodoListsController < ApplicationController
+	
 	def index
 		# send back a json collection of the todo lists
 		render json: TodoList.all
 	end
+
+	def show
+		list = TodoList.find(params[:id])
+		render json: list
+	end
+
 end
