7d577da90abe1949edf103c7f5aa94041e53ca91
Andreza Feu <andrezafeu@gmail.com>
2017-02-05 00:34:59 -0500

    Add update action for api

--- app/controllers/api/todo_lists_controller.rb
+++ app/controllers/api/todo_lists_controller.rb
@@ -29,6 +29,23 @@ class Api::TodoListsController < ApplicationController
 		end
 	end
 
+	def update
+		list = TodoList.find(params[:id])
+		if list.update(list_params)
+			render json: {
+				status: 200,
+				message: "Successfully updated list",
+				todo_list: list
+			}.to_json
+		else
+			render json: {
+				status: 500,
+				message: "List could not be updated",
+				todo_list: list
+			}.to_json
+		end
+	end
+
 	def destroy
 		list = TodoList.find(params[:id])
 		list.destroy
